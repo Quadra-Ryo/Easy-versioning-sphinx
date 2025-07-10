e2ad0b18a47f1a0ab9aa8048ee9de30e4676271a
Quadra-Ryo <niccoquadra@gmail.com>
2025-07-11 00:54:21 +0200

    Added the "add_versioning" function, which uses three helper functions to retrieve all useful project data and adds the footer script to all ".md" files in the project.

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ def initial_setup():
     """
     Prepare the workspace by cleaning old builds and copying source files.
     """
+
     try:
         # Cleaning the old build if exists
         if os.path.exists(f"{BUILD_PATH}/build"):
@@ -85,6 +86,7 @@ def final_cleaning():
     """
     Remove temporary folders after the build process.
     """
+
     try:
         if os.path.exists("_temp"):
             shutil.rmtree("_temp", onexc=handle_remove_readonly)
@@ -101,6 +103,7 @@ def get_versions():
     """
     Retrieve all available documentation versions from the source directory.
     """
+
     versions = []
     version_list_str = ""
 
@@ -145,6 +148,122 @@ def project_data_setup(versions):
     info(f"Updated footer with versions: {version_list}")    
     info(f"Updated footer with versions: {DEFAULT_LANGUAGE}")
 
+# Adding the actual versioning script to all the ".md" files
+def add_versioning(versions):
+    """
+    Append versioning information to all Markdown files.
+    """
+
+    version_languages = []
+
+    for version in versions:
+        info(f"Processing version: {version}")
+        # Retrieve available languages for the current version and update the footer accordingly
+        languages = languages_current_version_setup(version)
+        version_languages.append((version, languages))
+
+        for language in languages:
+            info(f"Adding versioning for: {version} / {language}")
+            # Adding the current language to the footer file
+            change_language(language)
+            lang_path = f"_temp/src/{version}/{language}"
+            # Getting all the ".md" files inside the folders
+            md_files = find_md(lang_path)
+
+            with open("_temp/data/temp_footer.md", "r") as footer_file:
+                footer_content = footer_file.read()
+
+            for md_file in md_files:
+                with open(md_file, "a") as file:
+                    file.write("\n\n\n" + footer_content)
+
+            success(f"Added footer to all Markdown files in: {version} / {language}")
+            # Going back to the placeholder after finishing with the current language
+            change_language(language)
+            
+    return version_languages
+
+# Retriving all the available languages for a specific version of the documentation
+def languages_current_version_setup(version):
+    """
+    Retrieve all available languages for a specific version.
+    """
+
+    info(f"Retriving the languages inside the version {version}")
+
+    html_languages = ""
+    languages = []
+    
+    # Creating a copy of the "standard project" footer where I'm gonna add only the current version/file data
+    shutil.copy("_temp/data/footer.md", "_temp/data/temp_footer.md")
+
+    language_list = "["
+    version_path = f"_temp/src/{version}"
+
+        
+    with open("_temp/data/temp_footer.md", "r") as footer_file:
+        footer = footer_file.read()
+
+    for folder in os.listdir(version_path):
+        if "_" not in folder:  # Exclude folders starting with "_"
+            languages.append(folder)
+            language_list += f'"{folder}", '
+
+    # HTML code for the languages, \t used to proprely indent
+    html_languages = "\n".join(
+        f"{'\t' if i == 0 else '\t\t\t\t'}<a href=\"#\" role=\"option\">{lang}</a>"
+        for i, lang in enumerate(languages)
+    )
+
+    # Removing the last wrong chars and closing the array
+    language_list = language_list.rstrip(", ") + "]"
+
+    # Replacing all the data of the current version of the documentation
+    footer = footer.replace("{version}", version)
+    footer = footer.replace("{html_l}", html_languages)
+    footer = footer.replace("{l_list}", language_list)
+    footer = footer.replace("{default_language}", DEFAULT_LANGUAGE)
+
+    with open("_temp/data/temp_footer.md", "w") as footer_file:
+        footer_file.write(footer)
+        
+    info(f"Found languages for version '{version}': {language_list}")
+    return languages
+
+# Function used to switch from the language placeholder to the current language and back to the placeholder
+def change_language(language):
+    """
+    Insert the selected language into the temporary footer file.
+    """
+
+    with open("_temp/data/temp_footer.md", "r") as footer_file:
+        footer = footer_file.read()
+
+    if("{language}" in footer):
+        footer = footer.replace("{language}", language)
+    else:
+        footer = footer.replace(f"{language}</s", "{language}</s")
+        
+    with open("_temp/data/temp_footer.md", "w") as footer_file:
+        footer_file.write(footer)
+
+# Function that search for all the ".md" files inside of a directory
+def find_md(directory):
+    """
+    Recursively find all Markdown files in a directory, excluding folders starting with '_'.
+    """
+
+    info(f"Retrieving all the \".md\" files inside the directory '{directory}'")
+
+    # Looping inside every directory that is not a "_" folder and getting every ".md" file path
+    md_files = []
+    for root, dirs, files in os.walk(directory):
+        dirs[:] = [d for d in dirs if not d.startswith("_")]
+        for file in files:
+            if file.endswith(".md"):
+                md_files.append(os.path.join(root, file))
+    return md_files
+
 ################################################################################## Main
 
 if __name__ == "__main__":
@@ -168,6 +287,10 @@ if __name__ == "__main__":
     info("Setting up the versions data")
     project_data_setup(versions)
     success("Setup ended")
+
+    info("Adding versioning to all the Markdown files")
+    version_languages = add_versioning(versions)
+    success("Versioning added to all Markdown files.")
 
     # Cleaning the project folders
     info("Final cleaning process")
