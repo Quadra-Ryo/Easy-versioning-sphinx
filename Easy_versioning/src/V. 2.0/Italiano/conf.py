2d560911fcf9a63e5cfca150f04780a723a6d2f7
Quadra-Ryo <niccoquadra@gmail.com>
2025-07-16 20:08:44 +0200

    Minor bug fixing, removed an old CSS file that was giving problems

--- Easy_versioning/src/V. 2.0/Italiano/conf.py
+++ Easy_versioning/src/V. 2.0/Italiano/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ARS Automation'
-copyright = '2025, ARS Automation'
-author = 'ARS Automation - Niccolò Quadrani'
-release = '1.0'
+project = 'Easy Versioning'
+copyright = '2025, Easy Versioning'
+author = 'Niccolò Quadrani'
+release = '2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -54,6 +54,3 @@ source_suffix = {
 #html_theme = 'sphinx_rtd_theme'
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
-html_css_files = [
-    'custom.css',
-]
