a083b95531dadd039606c11cc4cf2f9f5985279c
Quadra-Ryo <niccoquadra@gmail.com>
2025-07-20 01:15:58 +0200

    Build of the example project

--- Example/project/build/start_server.bat
+++ Example/project/build/start_server.bat
@@ -1,4 +1,4 @@
-cd "C:\Users\niccoloq\OneDrive - Ars Srl\Desktop\Private\Easy-versioning-sphinx\Example\project/build"
+cd "C:\Users\nicco\Desktop\Code\Easy-versioning-sphinx\Example\project/build"
 start /b python -m http.server 8000 --bind 0.0.0.0
 timeout /t 2 /nobreak
 explorer "http://localhost:8000/V. 2.0/English/index.html"
